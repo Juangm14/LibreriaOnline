1ac419cb4782729f9e31ecad678089727f1771c2
Alfonso Izura <aic29@gcloud.ua.es>
2021-04-13 17:10:10 +0200

    73439051M Version 1.0 de ENSoporte.cs

---
+++ EN/ENSoporte.cs
@@ -0,0 +1,126 @@
+﻿using System;
+
+public class ENSoporte
+{
+
+    private int id;
+    private string pregunta;
+    private string respuesta;
+
+    public int ID
+    {
+        get { return id; }
+        set { id = value; }
+    }
+
+    public string Pregunta
+    {
+        get { return pregunta; }
+        set { pregunta = value; }
+    }
+
+    public string Respuesta
+    {
+        get { return respuesta; }
+        set { respuesta = value; }
+    }
+
+    public ENSoporte()
+    {
+        Pregunta = "";
+        ID = -1;
+        Respuesta = "";
+    }
+
+    public ENSoporte(string pregunta, string respuesta, int id)
+    {
+        Pregunta = pregunta;
+        ID = id;
+        Respuesta = respuesta;
+    }
+
+    public bool readPregunta()
+    {
+        CADSoporte soporte = new CADSoporte();
+        return soporte.readPregunta(this);
+    }
+
+    public bool createPregunta()
+    {
+        CADSoporte soporte = new CADSoporte();
+
+        if (!soporte.readPregunta(this))
+        {
+            return soporte.createPregunta(this);
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public bool readFirstPregunta()
+    {
+        CADSoporte soporte = new CADSoporte();
+        return soporte.readFirstPregunta(this);
+    }
+    public bool readNextPregunta()
+    {
+        CADSoporte soporte = new CADSoporte();
+        if (soporte.readPregunta(this))
+        {
+            return soporte.readNextPregunta(this);
+        }
+        else
+        {
+            return false;
+        }
+    }
+    public bool readPrevPregunta()
+    {
+        CADSoporte soporte = new CADSoporte();
+        if (soporte.readPregunta(this))
+        {
+            return soporte.readPrevPregunta(this);
+        }
+        else
+        {
+            return false;
+        }
+    }
+    public bool editPregunta()
+    {
+        CADSoporte soporte = new CADSoporte();
+        ENSoporte copia = new ENSoporte();
+        copia.ID = ID;
+        copia.Pregunta = Pregunta;
+        copia.Respuesta = Respuesta;
+        if (soporte.readPregunta(this))
+        {
+            ID = copia.ID;
+            Pregunta = copia.Pregunta;
+            Respuesta = copia.Respuesta;
+            return soporte.editPregunta(this);
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public bool deletePregunta()
+    {
+        CADSoporte soporte = new CADSoporte();
+        if (soporte.readPregunta(this))
+        {
+            return soporte.deletePregunta(this);
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+
+}
+}
