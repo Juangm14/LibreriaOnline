500342e1b09c07eac250851a00074ac71471c1dc
Alfonso Izura <aic29@gcloud.ua.es>
2021-04-13 18:22:38 +0200

    73439051M Version 1.0 de CADRegistro.cs

---
+++ CAD/CADRegistro.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class CADRegistro
+{
+	private String constring;
+	public CADRegistro()
+	{
+	}
+	public ExisteEmail(ENRegistro registro)
+    {
+
+    }
+	public ExisteNick(ENRegistro registro)
+	{
+
+	}
+	public Registrar(ENRegistro registro)
+    {
+
+    }
+	public ReadEmail(ENRegistro registro)
+	{
+
+	}
+	public ReadNick(ENRegistro registro)
+	{
+
+	}
+
+}
+}
