57553dfc81ca7ad1467fe7862b57fbd80787c037
Benjamín Eduardo Cornelio Sorto <becs1@alu.ua.es>
2021-04-15 20:51:07 +0200

    Propiedades añadidas a ENListaUsuario

---
+++ ENListaUsus.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Almacena los libros que se ha leido el usuario con su puntuacion.
+/// </summary>
+public class ENListaUsuario
+{
+	private List<ENLibro> _libros;
+	private int _puntuacion;
+
+	public List<ENLibro> libros
+	{
+		get { return _libros; }
+		set { _libros = value; }
+	}
+	public int puntuacion
+    {
+		get { return _puntuacion; }
+		set { _puntuacion = value; }
+    }
+
+	public ENListaUsuario()
+	{
+		
+	}
+
+
+}
