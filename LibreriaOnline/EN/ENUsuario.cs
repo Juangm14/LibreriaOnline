7a893b009cb1aa04af618e3c1802ae6b523fb172
Pablo <pno2@gcloud.ua.es>
2021-04-16 13:53:41 +0200

    ENusuario

--- LibreriaOnline/EN/ENUsuario.cs
+++ LibreriaOnline/EN/ENUsuario.cs
@@ -6,7 +6,39 @@ using System.Threading.Tasks;
 
 namespace LibreriaOnline.EN
 {
-    class Class1
+    class ENUsuario
     {
+        private string email;
+        private string nick;
+        private string nombre;
+        private string apellidos;
+        private string telefono;
+        private int edad;
+        private string direccion;
+        private string contraseña;
+    }
+
+    public ENUsuario()
+    {
+        Email = "";
+        Nick = "";
+        Nombre = "";
+        Apellidos = "";
+        Telefono = "";
+        Edad = 0;
+        Direccion = "";
+        Contraseña = "";
+    }
+
+    public ENSugerencia(string email, string nick, string nombre, int edad, string telefono, string apellidos, string direccion, string contraseña)
+    {
+        Email = email;
+        Nick = nick;
+        Nombre = nombre;
+        Apellidos = apellidos;
+        Telefono = telefono;
+        Edad = edad;
+        Direccion = direccion;
+        Contraseña = contraseña;
     }
 }
