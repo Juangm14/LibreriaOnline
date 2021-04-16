7a74de6d11c94df30f9a7bc291d858e453794d63
Pablo <pno2@gcloud.ua.es>
2021-04-16 13:28:42 +0200

    Añadido CAD

---
+++ LibreriaOnline/CAD/CADSugerencias.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibreriaOnline.CAD
+{
+    class CADSugerencias
+    {
+        private ArrayList sugerencias = new ArrayList();
+        private string constring;
+
+
+        public CAD_Sugerencias{
+
+        }
+
+
+    public bool createSugerencia(ENSugerencias en)
+    {
+        return true;
+    }
+
+    public bool readSugerencia(ENSugerencias en)
+    {
+        return true;
+    }
+
+    public bool updateSugerencia(ENSugerencias en)
+    {
+        return true;
+    }
+
+    public bool deleteSugerencia(ENSugerencias en)
+    {
+        return true;
+    }
+}
