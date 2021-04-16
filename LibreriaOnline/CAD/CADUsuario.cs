270b996f3d5ae0032c488762394757a483f9313b
Pablo <pno2@gcloud.ua.es>
2021-04-16 15:20:51 +0200

    CAD Y EN completos

--- LibreriaOnline/CAD/CADUsuario.cs
+++ LibreriaOnline/CAD/CADUsuario.cs
@@ -3,10 +3,72 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LibreriaOnline.EN;
 
 namespace LibreriaOnline.CAD
 {
-    class Class1
+    class CADUsuario
     {
+        private string constring;
+        public CADUsuario()
+        {
+
+        }
+        public bool modificardatosUsuario(ENUsuario en)
+        {
+            return true;
+        }
+        public bool relatosUsuario(ENUsuario en)
+        {
+            return true;
+        }
+        public bool readrelatosUsuario(ENUsuario en)
+        {
+            return true;
+        }
+        public bool deleterelatosUsuario(ENUsuario en)
+        {
+            return true;
+        }
+        public bool venderUsuario(ENUsuario en)
+        {
+            return true;
+        }
+        public bool deletevenderUsuario(ENUsuario en)
+        {
+            return true;
+        }
+        public bool recomendarlibrosUsuario(ENUsuario en)
+        {
+            return true;
+        }
+        public bool criticaUsuario(ENUsuario en)
+        {
+            return true;
+        }
+        public bool readcriticaUsuario(ENUsuario en)
+        {
+            return true;
+        }
+        public bool deletecriticaUsuario(ENUsuario en)
+        {
+            return true;
+        }
+        public bool createUsuario(ENUsuario en)
+        {
+            return true;
+        }
+        public bool readUsuario(ENUsuario en)
+        {
+            return true;
+        }
+        public bool deleteUsuario(ENUsuario en)
+        {
+            return true;
+        }
+        public bool updateUsuario(ENUsuario en)
+        {
+            return true;
+        }
     }
 }
--- LibreriaOnline/EN/ENUsuario.cs
+++ LibreriaOnline/EN/ENUsuario.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LibreriaOnline.CAD;
 
 namespace LibreriaOnline.EN
 {
@@ -30,7 +31,7 @@ namespace LibreriaOnline.EN
         Contraseña = "";
     }
 
-    public ENSugerencia(string email, string nick, string nombre, int edad, string telefono, string apellidos, string direccion, string contraseña)
+    public ENUsuario(string email, string nick, string nombre, int edad, string telefono, string apellidos, string direccion, string contraseña)
     {
         Email = email;
         Nick = nick;
@@ -41,4 +42,87 @@ namespace LibreriaOnline.EN
         Direccion = direccion;
         Contraseña = contraseña;
     }
-}
+    public bool modificardatosUsuario()
+    {
+        CADUsuario u=new CADUsuario
+            return u.modificarUsuario(this);
+    }
+
+    public bool relatosUsuario()
+    {
+        CADUsuario u = new CADUsuario
+            return u.relatosUsuario(this);
+    }
+
+    public bool readrelatosUsuario()
+    {
+        CADUsuario u = new CADUsuario
+            return u.readrelatosUsuario(this);
+    }
+
+    public bool deleterelatosUsuario()
+    {
+        CADUsuario u= new CADUsuario
+            return u.deleterelatosUsuario(this);
+    }
+
+    public bool venderUsuario()
+    {
+        CADUsuario u = new CADUsuario
+            return u.venderUsuario(this);
+    }
+
+    public bool deletevenderUsuario()
+    {
+        CADUsuario u = new CADUsuario
+            return u.deletevenderUsuario(this);
+    }
+
+    public bool recomendarlibrosUsuario()
+    {
+        CADUsuario u = new CADUsuario
+            return u.recomendarlibrosUsuario(this);
+    }
+
+    public bool criticaUsuario()
+    {
+        CADUsuario u = new CADUsuario
+            return u.criticaUsuario(this);
+    }
+    
+    public bool readcriticaUsuario()
+    {
+        CADUsuario u = new CADUsuario
+            return u.readcriticaUsuario(this);
+    }
+
+    public bool deletecriticaUsuario()
+    {
+        CADUsuario u = new CADUsuario
+            return u.deletecriticaUsuario(this);
+    }
+    public bool createUsuario()
+    {
+        CADUsuario u = new CADUsuario
+            return u.readUsuario(this);
+    }
+
+    public bool readUsuario()
+    {
+        CADUsuario u = new CADUsuario
+            return u.modificarUsuario(this);
+    }
+
+    public bool deleteUsuario()
+    {
+        CADUsuario u = new CADUsuario
+            return u.deleteUsuario(this);
+    }
+
+    public bool updateUsuario()
+    {
+        CADUsuario u = new CADUsuario
+            return u.updateUsuario(this);
+    }
+    }
+
