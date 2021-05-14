9c2a9055411258176017def870e928738a008bdd
Alfonso Izura <aic29@gcloud.ua.es>
2021-05-14 18:25:01 +0200

    CADRegistro version 1.0

--- LibreriaOnline/LibreriaClases/CAD/CADRegistro.cs
+++ LibreriaOnline/LibreriaClases/CAD/CADRegistro.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace LibreriaOnline.CAD
 {
@@ -12,26 +14,128 @@ namespace LibreriaOnline.CAD
 		private String constring;
 		public CADRegistro()
 		{
+			constring = ConfigurationManager.ConnectionStrings["miconexion"].ToString();
 		}
+
 		public bool ExisteEmail(ENRegistro registro)
 		{
-			return true;
+			bool encontrado = false;
+            try
+            {
+				SqlConnection c = new SqlConnection(constring);
+				c.Open();
+				SqlCommand com = new SqlCommand("Select * from [dbo].[Usuario] Where email='"+registro.Email+"'", c);
+				SqlDataReader dr = com.ExecuteReader();
+				if (dr.Read())
+                {
+					encontrado = true;
+                }
+				dr.Close();
+				c.Close();
+			}
+			catch (SqlException e)
+			{
+				encontrado = false;
+				Console.WriteLine("User operation has failed.Error: {0}", e.Message);
+			}
+
+			return encontrado;
 		}
+
+
 		public bool ExisteNick(ENRegistro registro)
 		{
-			return true;
+			bool encontrado = false;
+			try
+			{
+				SqlConnection c = new SqlConnection(constring);
+				c.Open();
+				SqlCommand com = new SqlCommand("Select * from [dbo].[Usuario] Where nick='" + registro.Nick + "'", c);
+				SqlDataReader dr = com.ExecuteReader();
+				if (dr.Read())
+				{
+					encontrado = true;
+				}
+				dr.Close();
+				c.Close();
+			}
+			catch (SqlException e)
+			{
+				encontrado = false;
+				Console.WriteLine("User operation has failed.Error: {0}", e.Message);
+			}
+
+			return encontrado;
 		}
+
 		public bool Registrar(ENRegistro registro)
 		{
-			return true;
+			bool creado = false;
+			try
+			{
+				SqlConnection c = new SqlConnection(constring);
+				c.Open();
+				SqlCommand com = new SqlCommand("Insert INTO [dbo].[Usuario] (email, nick, contrasena, edad, telefono) VALUES ('" + registro.Email + "', '" + registro.Nick + "', '" + registro.Contrasena + "', "+registro.Edad+", "+registro.Telefono+ ")", c);
+				com.ExecuteNonQuery();
+				creado = true;
+				c.Close();
+			}
+			catch (SqlException e)
+			{
+				creado = false;
+				Console.WriteLine("User operation has failed.Error: {0}", e.Message);
+			}
+			return creado;
 		}
 		public bool ReadEmail(ENRegistro registro)
 		{
-			return true;
+			bool leido = false;
+			try
+			{
+				SqlConnection c = new SqlConnection(constring);
+				c.Open();
+				SqlCommand com = new SqlCommand("Select * from [dbo].[Usuario] Where email='" + registro.Email + "'", c);
+				SqlDataReader dr = com.ExecuteReader();
+				if (dr.Read())
+				{
+					registro.Contrasena = dr["contrasena"].ToString();
+					leido = true;
+				}
+				dr.Close();
+				c.Close();
+			}
+			catch (SqlException e)
+			{
+				leido = false;
+				Console.WriteLine("User operation has failed.Error: {0}", e.Message);
+			}
+
+			return leido;
 		}
+
 		public bool ReadNick(ENRegistro registro)
 		{
-			return true;
+			bool leido = false;
+			try
+			{
+				SqlConnection c = new SqlConnection(constring);
+				c.Open();
+				SqlCommand com = new SqlCommand("Select * from [dbo].[Usuario] Where nick='" + registro.Nick + "'", c);
+				SqlDataReader dr = com.ExecuteReader();
+				if (dr.Read())
+				{
+					registro.Contrasena = dr["contrasena"].ToString();
+					leido = true;
+				}
+				dr.Close();
+				c.Close();
+			}
+			catch (SqlException e)
+			{
+				leido = false;
+				Console.WriteLine("User operation has failed.Error: {0}", e.Message);
+			}
+			return leido;
 		}
 
 	}
