829e8e492962ec95f9903e0e31eccc587aa5fc6a
Alfonso Izura <aic29@gcloud.ua.es>
2021-05-22 13:19:32 +0200

    Añadido capacidad de cerrar sesion y menu dinamico

--- LibreriaOnline/Site1.Master.cs
+++ LibreriaOnline/Site1.Master.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,11 +9,37 @@ using System.Web.UI.WebControls;
 namespace LibreriaOnline {
     public partial class Site1 : System.Web.UI.MasterPage {
         protected void Page_Load(object sender, EventArgs e) {
-            
+
+            //bool existe = false;
+            if (Session["email"] != null && NavigationMenu.FindItem("Iniciar Sesion") != null)
+            {
+                NavigationMenu.Items.Remove(NavigationMenu.FindItem("Iniciar Sesion"));
+            }
+            if (Session["email"] == null && NavigationMenu.FindItem("") != null)
+            {
+                NavigationMenu.Items.Remove(NavigationMenu.FindItem(""));
+                /* IEnumerator menuItemEnumerator = engranaje.ChildItems.GetEnumerator();
+                 while (menuItemEnumerator.MoveNext())
+                 {
+                     if(((MenuItem)(menuItemEnumerator.Current)).ToolTip=="Cerrar Sesion")
+                     {
+                         existe = true;
+                     }
+                 }
+                 if (existe == true){
+                     NavigationMenu.FindItem("").ChildItems.RemoveAt(3);
+                 } 
+                */
+            }
+               
         }
 
         protected void NavigationMenu_MenuItemClick(object sender, MenuEventArgs e) {
-
+            if(e.Item.ToolTip== "Cerrar Sesion")
+            {
+                Session.Clear();
+                Response.Redirect("Home.aspx");
+            }
         }
     }
 }
