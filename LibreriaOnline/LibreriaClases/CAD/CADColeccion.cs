74e2b04e4182182c7cc5387c25d9048ff0f4f467
Benjamín Eduardo Cornelio Sorto <becs1@alu.ua.es>
2021-05-24 15:40:15 +0200

    04640493K Método addColeccion añadido a CADColeccion

--- LibreriaOnline/LibreriaClases/CAD/CADColeccion.cs
+++ LibreriaOnline/LibreriaClases/CAD/CADColeccion.cs
@@ -3,20 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
+using LibreriaOnline.EN;
 
-namespace ConsoleApp1
+namespace LibreriaOnline.CAD
 {
 	public class CADColeccion
 	{
-		private string costring;
+		private string constring;
 
 		public CADColeccion()
 		{
+			constring = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\LibreriaOnline.mdf;Integrated Security=True";
 		}
 
 		public bool addColeccion(ENColeccion en)
 		{
-			return true;
+			bool added = true;
+			SqlConnection c = new SqlConnection(constring);
+			try
+			{
+				c.Open();
+				SqlCommand com1 = new SqlCommand("Insert into Coleccion (id,nombre) VALUES ('" + en.id + "','" + en.nombre + "')", c);
+				SqlCommand com2 = new SqlCommand("Insert into ColeccionLibros (id,ISBN) VALUES ('" + en.id + "','" + en.coleccion + "')", c);
+				if (com1.ExecuteNonQuery() == 0 || com2.ExecuteNonQuery() == 0)
+					added = false;
+			}
+			catch (SqlException ex)
+			{
+				added = false;
+				Console.WriteLine("User operation has failed.Error: {0}", ex.Message);
+			}
+			finally { c.Close(); }
+			
+			return added;
 		}
 
 		public bool removeColeccion(ENColeccion en)
