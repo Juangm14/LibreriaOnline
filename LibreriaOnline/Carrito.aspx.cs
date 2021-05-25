5b842f6711e115aadeec5ac069416ecfb52f28be
Juan Garcia Martinez <jgm179@gcloud.ua.es>
2021-05-25 17:56:21 +0200

    Actualizando Relatos, para poder visualizarlos

---
+++ LibreriaOnline/Carrito.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LibreriaOnline {
+    public partial class Carrito : System.Web.UI.Page {
+        protected void Page_Load(object sender, EventArgs e) {
+            
+        }
+
+        protected void AgregarCarrito_Click(object sender, EventArgs e) {
+
+            if (Session["email"] != null) {
+
+
+            } else {
+
+            }
+        }
+    }
+}
