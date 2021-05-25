64438ec2a2fd96dd9d35a1cb9b812227a0dede42
Juan Garcia Martinez <jgm179@gcloud.ua.es>
2021-05-25 18:25:55 +0200

    Añadiendo nuevas funcionalidades Relatos

--- LibreriaOnline/LibreriaClases/CAD/CADRelato.cs
+++ LibreriaOnline/LibreriaClases/CAD/CADRelato.cs
@@ -1,84 +1,30 @@
-﻿using LibreriaOnline.EN;
+﻿using LibreriaClases.EN;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LibreriaOnline.CAD {
-    class CADRelato {
+namespace LibreriaClases.CAD {
+    public class CADRelato {
 
-        private String conexionRelato;
+        private string constring;
         public CADRelato() {
-            conexionRelato = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\LibreriaOnline.mdf;Integrated Security=True";
+            constring = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\LibreriaOnline.mdf;Integrated Security=True";
         }
 
-        public bool createRelato(ENRelato en) {
-            SqlConnection connection = new SqlConnection(conexionRelato);
+        public DataSet ListarRelatos(ENRelato en) {
+            DataSet bdvirtual = new DataSet();
 
-            try {
-                connection.Open();
-                SqlCommand com = new SqlCommand("Insert Into Relato (Titulo, genero, text, email) VALUES " +
-                                                "('" + en.getTitulo() + "','" + en.getGenero() + "','" + en.getTexto() + "','" + en.getUsuario() + "')", connection);
-                com.ExecuteNonQuery();
-                connection.Close();
-                return true;
-            } catch (SqlException ex) {
-                Console.WriteLine("User operation has failed.Error: {0}", ex.Message);
-                return false;
-            }
-        }
-        public bool deleteRelato(ENRelato en) {
-            SqlConnection connection = new SqlConnection(conexionRelato);
-            String sql = "Delete from Relato where Titulo = ('" + en.getTitulo() + "')";
-            try {
-                connection.Open();
-                SqlCommand c = new SqlCommand(sql, connection);
-                int filasAfectadas = c.ExecuteNonQuery();
-                connection.Close();
-                if (filasAfectadas > 0) return true;
-                else return false;
-            } catch (SqlException ex) {
-                Console.WriteLine("User operation has failed.Error: {0}", ex.Message);
-                return false;
-            }
-        }
-        public bool updateRelato(ENRelato en) {
-            SqlConnection connection = new SqlConnection(conexionRelato);
-            String sql = "update Relato set  Titulo = '{0}', genero = '{1}', text = '{2}' where Titulo = '{0}'";
-            try {
-                connection.Open();
-                SqlCommand c = new SqlCommand(string.Format(sql, new String[] { en.getTitulo(), en.getGenero().ToString(), en.getTexto() }), connection);
-                int filasAfectadas = c.ExecuteNonQuery();
-                connection.Close();
-                if (filasAfectadas > 0) return true;
-                else return false;
-            } catch (SqlException ex) {
-                Console.WriteLine("User operation has failed.Error: {0}", ex.Message);
-                return false;
-            }
-        }
-        public bool bucarRelato(ENRelato en) {
-            SqlConnection connection = new SqlConnection(conexionRelato);
-            String sql = "select Titulo from Relato where Titulo = '{0}'";
+            SqlConnection c = new SqlConnection(constring);
+            SqlDataAdapter da = new SqlDataAdapter("Select * from Relato where email != " + "('" + en.getEmail()  + "')", c);
+           
+            da.Fill(bdvirtual, "Relato");
 
-            try {
-                connection.Open();
-                SqlCommand c = new SqlCommand(string.Format(sql, new String[] { en.getTitulo() }), connection);
-                SqlDataReader reader = c.ExecuteReader();
-
-                if (reader.Read()) {
-                    if (reader["Titulo"].ToString().Equals(en.getTitulo())) return true;
-                } else { 
-                    return false;
-                }
-                connection.Close();
-            } catch (SqlException ex) {
-                Console.WriteLine("User operation has failed.Error: {0}", ex.Message);
-                return false;
-            }
-            return false;
+            return bdvirtual;
         }
+
     }
 }
--- LibreriaOnline/Relatos.aspx.cs
+++ LibreriaOnline/Relatos.aspx.cs
@@ -1,191 +1,40 @@
-﻿using LibreriaOnline.EN;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using LibreriaClases.EN;
+using LibreriaOnline.EN;
 
 namespace LibreriaOnline {
     public partial class Relatos : System.Web.UI.Page {
+
+        DataSet d = new DataSet();
+
         protected void Page_Load(object sender, EventArgs e) {
-            //Ocultamos el formulario de creacion de Relato
-            exampleFormControlInput1.Visible = false;
-            GeneroDesplegable.Visible = false;
-            exampleFormControlInput2.Visible = false;
-            TituloRelato.Visible = false;
-            TextoRelato.Visible = false;
-            GeneroRelato.Visible = false;
-            CrearRelatoButton.Visible = false;
-            LabelCrear.Visible = false;
-
-            //Ocultamos el formulario de modificacion de Relato
-            ModLabelTitulo.Visible = false;
-            ModBuscarTitulo.Visible = false;
-            ModButton.Visible = false;
-            ModDropDownList1.Visible = false;
-            ModGenero.Visible = false;
-            ModLabelTexto.Visible = false;
-            ModTextTexto.Visible = false;
-            ModTextTitulo.Visible = false;
-            ModTextNewTitulo.Visible = false;
-            ModLabelNewTitulo.Visible = false;
-            ModMsgBuscar.Visible = false;
-            ModLabelUpdateRelato.Visible = false;
-
-            //Ocultamos el formulario de eliminacion de Relato
-            ElimLabelRelato.Visible = false;
-            ElimTituloRelato.Visible = false;
-            ElimBotonRelato.Visible = false;
-            ElimMsg.Visible = false;
-
-            //Ocultamos la vista de los relatos
-            if (Session["email"] == null) {
-                GridRelatosShow.Visible = false;
-                NuevoRelato.Visible = false;
-                EditarRelato.Visible = false;
-                BorrarRelato.Visible = false;
-                MsgRelato.Visible = true;
-                MsgRelato.Text = "<br> Para tener el acceso completo a esta funcionalidad inicia sesion, ";
-                mensajeSession.Visible = true;
-            } else {
-                GridRelatosShow.Visible = true;
-                NuevoRelato.Visible = true;
-                EditarRelato.Visible = true;
-                BorrarRelato.Visible = true;
-                mensajeSession.Visible = false;
-            }
-
-        }
-
-        protected void NuevoRelato_Click(object sender, EventArgs e) {
-            exampleFormControlInput1.Visible = true;
-            GeneroDesplegable.Visible = true;
-            exampleFormControlInput2.Visible = true;
-            TituloRelato.Visible = true;
-            TextoRelato.Visible = true;
-            GeneroRelato.Visible = true;
-            CrearRelatoButton.Visible = true;
-            MsgRelato.Visible = false;
-            GridRelatosShow.Visible = false;
-        }
-
-
-        protected void EditarRelato_Click(object sender, EventArgs e) {
-
-            ModLabelTitulo.Visible = true;
-            ModTextTitulo.Visible = true;
-            ModBuscarTitulo.Visible = true;
-            GridRelatosShow.Visible = false;
-
-        }
-
-        protected void BuscarRelato_Click(object sender, EventArgs e) {
-            GridRelatosShow.Visible = false;
-            try {
-                ENRelato en = new ENRelato();
-                if (ModTextTitulo.Text.Length != 0) {
-                    en.setTitulo(ModTextTitulo.Text.ToString());
-
-                    if (en.bucarRelato()) {
-                        ModButton.Visible = true;
-                        ModDropDownList1.Visible = true;
-                        ModGenero.Visible = true;
-                        ModLabelTexto.Visible = true;
-                        ModTextTexto.Visible = true;
-                        ModTextNewTitulo.Visible = true;
-                        ModLabelNewTitulo.Visible = true;
-                    } else {
-                        ModMsgBuscar.Visible = true;
-                        ModMsgBuscar.Text = "<br> No existe ese Relato, intentalo de nuevo.";
-                    }
-                } else {
-                    ModMsgBuscar.Visible = true;
-                    ModMsgBuscar.Text = "<br>ERROR: No ha introducido ningun dato.";
-                }
-            } catch (Exception ex) {
-                ModMsgBuscar.Visible = true;
-                ModMsgBuscar.Text = "<br> ERROR: " + ex.Message.ToString();
-            }
-        }
-
-        protected void BorrarRelato_Click(object sender, EventArgs e) {
-            ElimLabelRelato.Visible = true;
-            ElimTituloRelato.Visible = true;
-            ElimBotonRelato.Visible = true;
-            GridRelatosShow.Visible = false;
-        }
-        protected void CrearNuevoRelato_Click(object sender, EventArgs e) {
-
-            String usuario = null;
-
-            if (Session["email"] != null) {
-
-                usuario = Session["email"].ToString();
-                if (exampleFormControlInput1.Text.Length != 0) {
-                    try {
-                        ENRelato en = new ENRelato(exampleFormControlInput1.Text.ToString(), GeneroDesplegable.Text.ToString(), exampleFormControlInput2.Text.ToString(), usuario);
-                        if (en.createRelato()) {
-                            LabelCrear.Visible = true;
-                            LabelCrear.Text = "<br> El relato se ha creado correctamente";
-                        } else {
-                            LabelCrear.Visible = true;
-                            LabelCrear.Text = "<br> El relato no se ha creado, intentalo de nuevo.";
-                        }
-                    } catch (FormatException ex) {
-                        LabelCrear.Visible = true;
-                        LabelCrear.Text = "<br> ERROR: " + ex.Message.ToString();
-                    }
-                } else {
-                    LabelCrear.Visible = true;
-                    LabelCrear.Text = "<br>ERROR: El relato no se ha creado, introduce un titulo.";
-                }
-            } else {
-                Response.Redirect("Signin.aspx");
-            }
-
-        }
-
-        protected void ModificarRelato_Click(object sender, EventArgs e) {
-            GridRelatosShow.Visible = false;
-            ENRelato en = new ENRelato(ModTextNewTitulo.Text.ToString(), ModDropDownList1.Text.ToString(), ModTextTexto.Text.ToString());
-            try {
-                if (en.updateRelato()) {
-                    ModLabelUpdateRelato.Visible = true;
-                    ModLabelUpdateRelato.Text = "<br>El relato ha sido modificado correctamente.";
-                } else {
-                    ModLabelUpdateRelato.Visible = true;
-                    ModLabelUpdateRelato.Text = "<br>El relato no ha podido ser modificado, intentelo de nuevo.";
-                }
-            } catch (FormatException ex) {
-                ModLabelUpdateRelato.Visible = true;
-                ModLabelUpdateRelato.Text = "<br> ERROR: " + ex.Message.ToString();
-            }
-
-        }
-
-        protected void EliminarRelato_Click(object sender, EventArgs e) {
-            GridRelatosShow.Visible = false;
-            ElimMsg.Visible = true;
-            if (ElimTituloRelato.Text.Length != 0) {
+            LabelError.Visible = false;
+            mensajeSession.Visible = false;
+            if (!Page.IsPostBack) {
                 try {
-                    ENRelato en = new ENRelato();
-                    en.setTitulo(ElimTituloRelato.Text.ToString());
-                    if (en.deleteRelato()) {
-                        ElimMsg.Visible = true;
-                        ElimMsg.Text = "<br>Relato eliminado correctamente.";
+                    if (Session["Email"] != null) {
+                        LabelError.Visible = false;
+                        ENRelato en = new ENRelato(Session["email"].ToString());
+
+                        d = en.ListarRelato();
+                        GridRelato.DataSource = d;
+                        GridRelato.DataBind();
                     } else {
-                        ElimMsg.Visible = true;
-                        ElimMsg.Text = "<br>El relato no se ha podido eliminar.";
+                        LabelError.Visible = true;
+                        LabelError.Text = "<br> Para tener el acceso completo a esta funcionalidad inicia sesion, ";
+                        mensajeSession.Visible = true;
                     }
-                } catch (FormatException ex) {
-                    ElimMsg.Visible = true;
-                    ElimMsg.Text = "<br> ERROR: " + ex.Message.ToString();
+                } catch (Exception ex) {
+                    LabelError.Visible = true;
+                    LabelError.Text = "<br> ERROR: " + ex.Message.ToString();
                 }
-            } else {
-                ElimMsg.Visible = true;
-                ElimMsg.Text = "<br>ERROR: Introduce el titulo del relato a borrar.";
             }
         }
-    }
+    } 
 }
