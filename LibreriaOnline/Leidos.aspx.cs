cee262f82460c34aa5dbda3c28a8bbe05e3c757b
Benjamín Eduardo Cornelio Sorto <becs1@alu.ua.es>
2021-06-04 18:18:04 +0200

    Page_load añadido

--- LibreriaOnline/Leidos.aspx.cs
+++ LibreriaOnline/Leidos.aspx.cs
@@ -7,8 +7,38 @@ using System.Web.UI.WebControls;
 
 namespace LibreriaOnline {
     public partial class Leidos : System.Web.UI.Page {
-        protected void Page_Load(object sender, EventArgs e) {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Msg.Visible = true;
+            LabelLibro.Visible = false;
+            LabelNota.Visible = false;
+            ListaLibros.Visible = false;
+            ListaPuntuacion.Visible = false;
 
+            //Creación de leídos
+            AñadirPuntuacion.Visible = false;
+
+            //Modificación de leídos
+            ModificarPuntuacion.Visible = false;
+
+            //Eliminación de leídos
+            EliminarPuntuacion.Visible = false;
+
+            //Grid de leídos
+            if (Session["email"] == null)
+            {
+                GridLeidos.Visible = false;
+                NuevoLibroLeido.Visible = false;
+                ModLibroLeido.Visible = false;
+                ElimLibroLeido.Visible = false;
+            }
+            else
+            {
+                GridLeidos.Visible = true;
+                NuevoLibroLeido.Visible = true;
+                ModLibroLeido.Visible = true;
+                ElimLibroLeido.Visible = true;
+            }
         }
     }
 }
